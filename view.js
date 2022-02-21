f726ea062bda42f29187b5284502838a667d49cc
Kristian Roland <kristian@rolandtech.org>
2022-02-21 11:54:01 +0100

    Update view.js

--- view.js
+++ view.js
@@ -8,7 +8,6 @@ function createDiscView(y) {
             <img class="discImage" src="./discbilder/${disc.image}" />
             <div>${disc.spec}</div>
         </div>
-        
         `;
 	}
 }
@@ -20,42 +19,36 @@ function showView() {
     <button onclick="buyDisks()">Kjøp disker</button> <button onclick="undo()">Angre forrige</button>
     <div>Du har: ${cart ? cart.length : ''} disker i handlevognen</div>
     <div class="discContainer">${discView}</div><br>
-   
-    
-    
     `;
 }
 
 function showDisc(disc) {
 	document.getElementById('app').innerHTML = /*HTML*/ `
-    <button onclick="showView()">Home</button> <button onclick="toCart(disc)">Legg i Handlevogn</button>
+    <button onclick="showView()">Hjem</button><br>
+    <button onclick="toCart(disc)">Legg i Handlevogn</button>
     <div class="chosenDiscView">
     <div>${disc.name}</div>
     <img class="singleDisk" src="./discbilder/${disc.image}" />
     <div>Spec: ${disc.spec}</div>
     <div>Vekt: ${disc.weight}</div>    
     </div>
-
     `;
 }
 
 function showBoughtDisks() {
 	createDiscView(cart);
 	document.getElementById('app').innerHTML = /*HTML*/ `
-    <button onclick="undoAll()">Angre kjøpet</button><button onclick="location.href = 'https://www.youtube.com/watch?v=Gc2u6AFImn8';" >Ut av butikken</button>
+    <button onclick="location.href = 'https://www.youtube.com/watch?v=Gc2u6AFImn8';" >Ut av butikken</button> <button onclick="undoAll()">Angre kjøpet</button>
     <div>Du har kjøpt: ${
 			cart ? cart.length : ''
 		} disker, takk for handelen!</div>
     <div class="discContainer">${discView}</div><br>
-    
     `;
 }
 
 function showUndoAll() {
 	document.getElementById('app').innerHTML = /*HTML*/ `
-    
-    <div>Du har angret kjøpet</div>
     <button onclick="showView()">Hjem</button>
-    
+    <div>Du har angret kjøpet</div>
     `;
 }
